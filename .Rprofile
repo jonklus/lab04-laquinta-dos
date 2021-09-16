0160498c500a9382980ac91b58234d75934549d2
Andrew McDavid <andrew.n.mcdavid@gmail.com>
2021-09-16 14:34:52 -0400

    Lint with lint_assignment()
    Lint github acction

---
+++ .Rprofile
@@ -0,0 +1,1 @@
+source('lintr/lint.R')
--- haversine.R
+++ haversine.R
@@ -5,10 +5,11 @@ haversine = function(long1, lat1, long2, lat2, round = 3) {
   long2 = long2 * pi / 180
   lat2  = lat2  * pi / 180
   
-  R = 6371 # Earth mean radius in km
+  earth_r = 6371 # Earth mean radius in km
   
-  a = sin((lat2 - lat1)/2)^2 + cos(lat1) * cos(lat2) * sin((long2 - long1)/2)^2
-  d = R * 2 * asin(sqrt(a))
+  a = sin((lat2 - lat1) / 2) ^ 2 + cos(lat1) * cos(lat2) * sin((long2 - long1) /
+                                                                 2) ^ 2
+  d = earth_r * 2 * asin(sqrt(a))
   
-  return( round(d,round) ) # distance in km
+  return(round(d, round)) # distance in km
 }
