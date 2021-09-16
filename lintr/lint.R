9ee82664ccbd8c0ad7ca926d220ec8bc7d0acce5
Andrew McDavid <andrew.n.mcdavid@gmail.com>
2021-09-16 15:16:35 -0400

    Back to installing lintr with a step.
    Remove single_quotes linter
    prevaricate about if we should `require` lintr or not.

--- lintr/lint.R
+++ lintr/lint.R
@@ -1,9 +1,9 @@
-if(require(lintr)) {
+if (require("lintr")) {
   lint_assignment = function() {
     arrow_assign = function(source_file) {
-      lapply(ids_with_token(source_file, "LEFT_ASSIGN"), function(id) {
-        parsed = with_id(source_file, id)
-        Lint(
+      lapply(lintr::ids_with_token(source_file, "LEFT_ASSIGN"), function(id) {
+        parsed = lintr::with_id(source_file, id)
+        lintr::Lint(
           filename = source_file$filename,
           line_number = parsed$line1,
           column_number = parsed$col1,
@@ -15,9 +15,10 @@ if(require(lintr)) {
       })
     }
     
-    linters = with_defaults(
+    linters = lintr::with_defaults(
       assignment_linter = NULL,
       arrow_assign = arrow_assign,
+      single_quotes_linter = NULL,
       trailing_whitespace_linter = NULL,
       line_length_linter(90),
       commented_code_linter = NULL,
